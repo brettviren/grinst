481437d90c3c825f55b41716adce68f850688dcf
Brett Viren <bv@bnl.gov>
2012-06-11 09:58:42 -0400

    Explicitly setup dependencies.

--- grinst_setup_g4lbne.sh
+++ grinst_setup_g4lbne.sh
@@ -3,6 +3,12 @@
 grinst_setup_g4lbne () {
     local prefix=$1; shift
 
+    for dep in python ipython root geant4
+    do
+	eval $(grinst_setup $dep)
+	grinst_setup $dep
+    done
+
     export PATH=$(pathadd $prefix/bin $PATH)
     export LD_LIBRARY_PATH=$(pathadd $prefix/bin $LD_LIBRARY_PATH)
     cat <<EOF
