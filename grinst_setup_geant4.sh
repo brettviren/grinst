4ba9f198c2686d1befe4617f2e857e4d454be22b
Brett Viren <bv@bnl.gov>
2012-12-14 12:46:32 -0500

    Add cowbells and make G4 data pseudoautodetected.

--- grinst_setup_geant4.sh
+++ grinst_setup_geant4.sh
@@ -9,18 +9,61 @@ grinst_setup_geant4 () {
     LD_LIBRARY_PATH=$(pathadd $prefix/lib64 $LD_LIBRARY_PATH)
     LD_LIBRARY_PATH=$(pathadd $prefix/lib $LD_LIBRARY_PATH)
 
+    
+
     cat <<EOF
 export PATH=$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-
-export G4ABLADATA="$geant4_data/G4ABLA3.0"
-export G4LEDATA="$geant4_data/G4EMLOW6.23"
-export G4LEVELGAMMADATA="$geant4_data/PhotonEvaporation2.2"
-export G4NEUTRONHPDATA="$geant4_data/G4NDL4.0"
-export G4NEUTRONXSDATA="$geant4_data/G4NEUTRONXS1.1"
-export G4PIIDATA="$geant4_data/G4PII1.3"
-export G4RADIOACTIVEDATA="$geant4_data/RadioactiveDecay3.4"
-export G4REALSURFACEDATA="$geant4_data/RealSurface1.0"
-
 EOF
+
+    local datadir=$(ls -d $prefix/share/*/data | tail -1)
+
+    dir=$(ls -d $geant4_data/G4ABLA* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4ABLADATA=$dir
+	echo "export G4ABLADATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/G4EMLOW* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4LEDATA=$dir
+	echo "export G4LEDATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/PhotonEvaporation* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4LEVELGAMMADATA=$dir
+	echo "export G4LEVELGAMMADATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/G4NDL** | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4NEUTRONHPDATA=$dir
+	echo "export G4NEUTRONHPDATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/G4NEUTRONXS* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4NEUTRONXSDATA=$dir
+	echo "export G4NEUTRONXSDATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/PII* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4PIIDATA=$dir
+	echo "export G4PIIDATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/RadioactiveDecay* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4RADIOACTIVEDATA=$dir
+	echo "export G4RADIOACTIVEDATA=$dir"
+    fi
+
+    dir=$(ls -d $geant4_data/RealSurface* | tail -1)
+    if [ -n "$dir" ] ; then 
+	G4REALSURFACEDATA=$dir
+	echo "export G4REALSURFACEDATA=$dir"
+    fi
+
 }
