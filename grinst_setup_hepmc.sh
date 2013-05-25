a58d6cf3878d95a035a1daacb4d56ba2f35cc983
Brett Viren <brett.viren@gmail.com>
2013-05-25 19:22:04 -0400

    Overzelous with escaping dollars.

--- grinst_setup_hepmc.sh
+++ grinst_setup_hepmc.sh
@@ -3,7 +3,7 @@
 grinst_setup_hepmc () {
     local prefix=$1; shift
     cat <<EOF
-export PATH=\$(pathadd \$prefix/bin \$PATH)
-export LD_LIBRARY_PATH=\$(pathadd \$prefix/lib \$LD_LIBRARY_PATH)    
+export PATH=\$(pathadd $prefix/bin \$PATH)
+export LD_LIBRARY_PATH=\$(pathadd $prefix/lib \$LD_LIBRARY_PATH)    
 EOF
 }
