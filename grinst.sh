580778040e7a94dfea3224243b682668a72f0981
Brett Viren <bv@bnl.gov>
2013-08-22 17:43:47 -0400

    Remove option to mktemp not supported by older versions.

--- grinst.sh
+++ grinst.sh
@@ -2,7 +2,7 @@
 
 grinst_dir=$(readlink -f $(dirname $BASH_SOURCE))
 
-grinst_setup_env_dir=$(mktemp --tmpdir=/tmp -d grinst.$USER.XXXXXXXX)
+grinst_setup_env_dir=$(mktemp -d grinst.$USER.XXXXXXXX)
 
 source $grinst_dir/grinst_util.sh
 source $grinst_dir/grinst_env_util.sh
